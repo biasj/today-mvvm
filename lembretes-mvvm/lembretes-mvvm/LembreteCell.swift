5119247b87f74e60164d42d32375f36da5f08fa7
Beatriz Sato <50559620+biasj@users.noreply.github.com>
2021-12-12 22:25:03 -0300

    Adiciona camada view

--- lembretes-mvvm/lembretes-mvvm/LembreteCell.swift
+++ lembretes-mvvm/lembretes-mvvm/LembreteCell.swift
@@ -5,4 +5,17 @@
 //  Created by Beatriz Sato on 12/12/21.
 //
 
-import Foundation
+import UIKit
+
+class LembreteCell: UITableViewCell {
+    typealias AcaoBotaoFeito = () -> Void
+    
+    @IBOutlet var tituloLabel: UILabel!
+    @IBOutlet var dataLabel: UILabel!
+    
+    var acaoBotaoFeito: AcaoBotaoFeito?
+    
+    @IBAction func botaoFeitoApertado(_ sender: UIButton) {
+        acaoBotaoFeito?()
+    }
+}
